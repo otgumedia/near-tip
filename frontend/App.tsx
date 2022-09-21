d971fdcab7b097f7697c14757252d478269eb4d5
NathanNicholson <thenathannicholson@gmail.com>
2022-09-20 17:54:02 -0700

    Inital commit

---
+++ frontend/App.tsx
@@ -0,0 +1,15 @@
+import "regenerator-runtime/runtime";
+
+import React from "react";
+
+import "./assets/global.css";
+
+const App = () => {
+  return (
+    <div>
+      <div>App</div>
+    </div>
+  );
+};
+
+export default App;
