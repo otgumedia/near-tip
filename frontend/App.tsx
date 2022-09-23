400ac9fec154106226e48dfcab4bb59aa5ba41e0
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2022-09-23 10:16:55 -0700

    Start building the frontend near-interface

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,11 +3,13 @@ import "regenerator-runtime/runtime";
 import React from "react";
 
 import "./assets/global.css";
+import { utils } from "near-api-js";
 
 const App = () => {
+  const num = 5;
   return (
     <div>
-      <div>App</div>
+      <div>{utils.format.parseNearAmount(num.toString())}</div>
     </div>
   );
 };
